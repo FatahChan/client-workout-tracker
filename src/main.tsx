84fa92325d84e476ef2078b9dd19e3a5e88e618c
FatahChan <ahmadfathallahh89@gmail.com>
2024-09-19 16:56:45 +0300

    fix: Not Found

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NotFound from "./components/NotFound";
 const queryClient = new QueryClient();
 
 // Create a new router instance
