2638a3f752b7256ce4922be0385b298593f0aad9
FatahChan <ahmadfathallahh89@gmail.com>
2024-09-24 04:31:10 +0300

    fix: login

--- src/lib/appwrite/util.ts
+++ src/lib/appwrite/util.ts
@@ -4,17 +4,27 @@ import { account } from ".";
  * @description Check if the user is logged in and throw an error if not otherwise return the user
  * @returns user
  */
-async function checkIfUserIsLoggedIn(safe = false) {
+async function checkIfUserIsLoggedIn() {
   let user;
+  let session;
   try {
     user = await account.get();
     if (!user?.$id) {
       throw new Error("User is not logged in");
     }
-  } catch {
-    if (!safe) {
+    session = await account.getSession("current");
+    window.localStorage.setItem("user", JSON.stringify(user));
+    window.localStorage.setItem("session", JSON.stringify(session));
+  } catch (error) {
+    if (window.navigator.onLine) {
+      console.log(error);
       throw new Error("User is not logged in");
     }
+    user = JSON.parse(window.localStorage.getItem("user") ?? "{}");
+    if (!user.$id) {
+      throw new Error("User is not logged in");
+    }
+    session = JSON.parse(window.localStorage.getItem("session") ?? "{}");
   }
   return user;
 }
